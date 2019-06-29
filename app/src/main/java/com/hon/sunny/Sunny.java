b3bc47f8d078b0cfb4eec4355f24beeaee323fb3
frankhon <frank_hon@foxmail.com>
2019-06-29 12:39:58 +0800

    fix bugs(maybe)

--- app/src/main/java/com/hon/sunny/Sunny.java
+++ app/src/main/java/com/hon/sunny/Sunny.java
@@ -3,17 +3,20 @@ package com.hon.sunny;
 import android.app.Application;
 import android.content.Context;
 
-import androidx.appcompat.app.AppCompatDelegate;
-
 import com.hon.sunny.utils.CrashHandler;
+import com.hon.sunny.utils.PLog;
+
+import java.io.IOException;
+import java.net.SocketException;
+
+import androidx.appcompat.app.AppCompatDelegate;
+import io.reactivex.exceptions.UndeliverableException;
+import io.reactivex.plugins.RxJavaPlugins;
 
 /**
  * Created by Frank on 2017/10/27.
  * E-mail:frank_hon@foxmail.com
- * <p>
- * project architecture:https://github.com/googlesamples/android-architecture/tree/todo-mvp/
  */
-
 public class Sunny extends Application {
     private static String sCacheDir;
     private static Context sAppContext;
@@ -48,6 +51,35 @@ public class Sunny extends Application {
         } else {
             sCacheDir = getApplicationContext().getCacheDir().toString();
         }
+
+        //https://github.com/ReactiveX/RxJava/wiki/What's-different-in-2.0#error-handling
+        // 统一处理RxJava的uncaught exception
+        RxJavaPlugins.setErrorHandler(e -> {
+            if (e instanceof UndeliverableException) {
+                e = e.getCause();
+            }
+            if ((e instanceof SocketException) || (e instanceof IOException)) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                return;
+            }
+            if (e instanceof InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                return;
+            }
+            if ((e instanceof NullPointerException) || (e instanceof IllegalArgumentException)) {
+                // that's likely a bug in the application
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+            if (e instanceof IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().getUncaughtExceptionHandler()
+                        .uncaughtException(Thread.currentThread(), e);
+                return;
+            }
+            PLog.w("Undeliverable exception received, not sure what to do", e.getMessage());
+        });
     }
 
     private boolean ExistSDCard() {
