8aca510de26bbcaf0b19ae841cccc488e62fa4d1
v-shhong <v-shhong@microsoft.com>
2019-09-19 13:24:51 +0800

    fix 首页item动画duration的bug

--- app/src/main/java/com/hon/sunny/ui/common/AnimatorAdapter.java
+++ app/src/main/java/com/hon/sunny/ui/common/AnimatorAdapter.java
@@ -6,9 +6,8 @@ import android.view.animation.DecelerateInterpolator;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.hon.sunny.ui.common.SimpleAnimationListener;
-
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Frank on 2017/8/10.
@@ -21,23 +20,23 @@ public abstract class AnimatorAdapter<T extends RecyclerView.ViewHolder>
     private static final int DELAY = 338;
     private int mLastPosition = -1;
     // to control items' animation duration
-    private LinkedList<ObjectAnimator> mAnimatorQueue = new LinkedList<>();
+    private List<ObjectAnimator> mAnimatorList = new ArrayList<>();
 
     protected void addItemAnimation(RecyclerView.ViewHolder holder) {
         int position = holder.getLayoutPosition();
         if (position > mLastPosition) {
             ObjectAnimator slideInFromLeft = ObjectAnimator.ofFloat(holder.itemView, "translationX", -800, 0);
             slideInFromLeft.setInterpolator(new DecelerateInterpolator());
-            slideInFromLeft.setDuration(DELAY * (mAnimatorQueue.size() + 1));
+            slideInFromLeft.setDuration(DELAY * (mAnimatorList.size() + 1));
             slideInFromLeft.addListener(new SimpleAnimationListener() {
                 @Override
                 public void onAnimationStart(Animator animation) {
-                    mAnimatorQueue.add(slideInFromLeft);
+                    mAnimatorList.add(slideInFromLeft);
                 }
 
                 @Override
                 public void onAnimationEnd(Animator animation) {
-                    mAnimatorQueue.pop();
+                    mAnimatorList.remove(slideInFromLeft);
                 }
             });
             slideInFromLeft.start();
@@ -47,6 +46,10 @@ public abstract class AnimatorAdapter<T extends RecyclerView.ViewHolder>
 
     protected void changeData() {
         mLastPosition = -1;
+        for (ObjectAnimator animator : mAnimatorList) {
+            animator.cancel();
+        }
+        mAnimatorList.clear();
         notifyDataSetChanged();
     }
 }
